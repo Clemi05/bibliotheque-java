e7472319d65547c0bf31d01db9ed474bcb5f2bc8
Valentine <you@example.com>
2022-08-31 10:28:00 +0200

    FirstCommit

---
+++ src/main/script/drop.sql
@@ -0,0 +1,6 @@
+drop table if exists devise CASCADE ;
+drop table if exists devise CASCADE ;
+drop table if exists devise CASCADE ;
+drop table if exists devise CASCADE ;
+drop table if exists devise CASCADE ;
+drop table if exists devise CASCADE ;
